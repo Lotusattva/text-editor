0cdfcd093ce36267915e2357f8b6b102e973d0f3
Jingwei Chen <cjw990802@gmail.com>
2025-05-01 21:21:06 -0700

    counter example

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
-use iced::widget::{button, column, text};
-use iced::Element;
+use iced::widget::{button, column, container, text};
+use iced::{alignment, Element, Fill};
 
 #[derive(Debug, Clone)]
 enum Message {
     Increment,
+    Decrement,
 }
 
 #[derive(Default)]
 struct Counter {
-    value: u64,
+    value: i32,
 }
 
 fn update(counter: &mut Counter, message: Message) {
     match message {
         Message::Increment => counter.value += 1,
+        Message::Decrement => counter.value -= 1,
     }
 }
 
 fn view(counter: &Counter) -> Element<Message> {
-    column![
-        text(counter.value).size(20),
-        button("Increment").on_press(Message::Increment),
-    ]
-    .spacing(10)
+    container(
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(counter.value)
+                .size(20)
+                .center()
+                .align_x(alignment::Horizontal::Center),
+            button("Decrement").on_press(Message::Decrement),
+        ]
+        .spacing(10),
+    )
+    .padding(20)
+    .center_x(Fill)
+    .center_y(Fill)
     .into()
 }
 
