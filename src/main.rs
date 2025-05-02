71b48b5063574759254d7fefb320cf2c472a34eb
Jingwei Chen <cjw990802@gmail.com>
2025-05-01 21:09:10 -0700

    trying

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-include!("counter.rs");
+use iced::widget::{button, column, text};
+use iced::Element;
 
-fn main() {
-    let mut counter = Counter::new(0);
-    counter.update(Message::Increment);
-    counter.update(Message::Increment);
-    counter.update(Message::Decrement);
-    println!("Counter value: {}", counter.val);
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+#[derive(Default)]
+struct Counter {
+    value: u64,
+}
+
+fn update(counter: &mut Counter, message: Message) {
+    match message {
+        Message::Increment => counter.value += 1,
+    }
+}
+
+fn view(counter: &Counter) -> Element<Message> {
+    column![
+        text(counter.value).size(20),
+        button("Increment").on_press(Message::Increment),
+    ]
+    .spacing(10)
+    .into()
+}
+
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", update, view)
 }
