78a058d489c89d187ef1dc9c4d12d7e2d63b446e
Jingwei Chen <cjw990802@gmail.com>
2025-05-03 16:27:06 -0700

    editor window

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,35 @@
-use iced::widget::{button, column, container, text};
-use iced::{alignment, Element, Fill};
+use iced::widget::{container, text_editor};
+use iced::{Element, Length};
 
-#[derive(Debug, Clone)]
-enum Message {
-    Increment,
-    Decrement,
+pub fn main() -> iced::Result {
+    iced::run("Text editor", update, view)
 }
 
 #[derive(Default)]
-struct Counter {
-    value: i32,
+struct MyEditor {
+    content: text_editor::Content,
 }
 
-fn update(counter: &mut Counter, message: Message) {
-    match message {
-        Message::Increment => counter.value += 1,
-        Message::Decrement => counter.value -= 1,
-    }
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
 }
 
-fn view(counter: &Counter) -> Element<Message> {
+fn view(editor: &MyEditor) -> Element<'_, Message> {
     container(
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(counter.value)
-                .size(20)
-                .center()
-                .align_x(alignment::Horizontal::Center),
-            button("Decrement").on_press(Message::Decrement),
-        ]
-        .spacing(10),
+        text_editor(&editor.content)
+            .placeholder("Start typing...")
+            .on_action(Message::Edit)
+            .height(Length::Fill),
     )
-    .padding(20)
-    .center_x(Fill)
-    .center_y(Fill)
+    .padding(10)
     .into()
 }
 
-pub fn main() -> iced::Result {
-    iced::run("A cool counter", update, view)
+fn update(editor: &mut MyEditor, message: Message) {
+    match message {
+        Message::Edit(action) => {
+            editor.content.perform(action);
+        }
+    }
 }
